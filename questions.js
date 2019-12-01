74f183156aaa434148de202427b23b33db20acf2
Bobby Odian <odianrobert@yahoo.com>
2019-11-30 22:26:37 -0500

    questions

---
+++ questions.js
@@ -0,0 +1,37 @@
+alert ("Click to start the quiz!!!");
+var title = questions;
+var choices = choices;
+var answer = answer;
+
+
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "What is the purpose of console.log?",
+        choices: ["prompt a user", "to split wood", "alert a user", "print text to console"],
+        answer: "print text to console"
+    },
+    {
+        title: "How do we perform a task in Javascript?",
+        choices: ["for loop", "a function", "an array", "math.random" ],
+        answer: "a function"
+    },
+    {
+        title: "What word is used to state if a condition is true, do this, else, do something else?",
+        choices: ["if", "alert", "while", "loop"],
+        answer: "if"
+    }
+  ];
+  
+  for(var i =0; i < questions.length; i++) {
+      var response = window.prompt(choices [i].prompt)
+  }
